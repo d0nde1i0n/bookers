5d6fa70d4c13466854af734e8cab3c74e54173ad
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-10 14:42:32 +0000

    [Add] 3-12  フラッシュメッセージの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root :to =>'homes#top' # アプリケーショントップ画面をルートパスへ
+  get 'books'=>'books#index'
+  post 'books'=>'books#create'
+  # lists/1や.../lists/3に該当する
+  get 'books/:id'=>'books#show', as: 'book'
+  get 'books/:id/edit'=>'books#edit',as: 'book-edit'
+  patch 'books/:id' => 'books#update',as: 'book-update'
+  delete 'books/:id' => 'books#destroy',as: 'book-destroy'
+
+
 end
